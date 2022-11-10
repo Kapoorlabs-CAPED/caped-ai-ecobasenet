8e1d00a4c45d90ef46118ff898f15f15a8dd1f14
kapoorlab <randomaccessiblekapoor@gmail.com>
2022-11-10 15:34:32 +0100

    dict

--- src/caped_ai_ecobasenet/config_imagenet.py
+++ src/caped_ai_ecobasenet/config_imagenet.py
@@ -42,13 +42,24 @@ class ImageNetConfig:
 def to_json(config: ImageNetConfig):
 
     is_valid(config)
+    config = {
+        "epochs": config.params_train.epochs,
+        "learning_rate": config.params_train.learning_rate,
+        "batch_size": config.params_train.batch_size,
+        "model_dir": config.paths_imagenet.model_dir,
+        "image_dir": config.paths_imagenet.image_dir,
+        "label_dir": config.paths_imagenet.label_dir,
+        "train_file": config.files_imagenet.train_file,
+        "val_file": config.files_imagenet.val_file,
+        "test_file": config.files_imagenet.test_file,
+    }
 
     save_json(
         config, config.paths_imagenet.model_dir + "/" + "parameters.json"
     )
 
 
-def is_valid(config: ImageNetConfig, return_invalid=False):
+def is_valid(config: dict, return_invalid=False):
     """Check if configuration is valid.
     Returns
     -------
@@ -57,15 +68,15 @@ def is_valid(config: ImageNetConfig, return_invalid=False):
     """
 
     ok = {}
-    ok["epochs"] = isinstance(config.params_train.epochs, int)
-    ok["learning_rate"] = isinstance(config.params_train.learning_rate, float)
-    ok["batch_size"] = isinstance(config.params_train.batch_size, int)
-    ok["model_dir"] = isinstance(config.paths_imagenet.model_dir, str)
-    ok["image_dir"] = isinstance(config.paths_imagenet.image_dir, str)
-    ok["label_dir"] = isinstance(config.paths_imagenet.label_dir, str)
-    ok["train_file"] = isinstance(config.files_imagenet.train_file, str)
-    ok["val_file"] = isinstance(config.files_imagenet.val_file, str)
-    ok["test_file"] = isinstance(config.files_imagenet.test_file, str)
+    ok["epochs"] = isinstance(config["epochs"], int)
+    ok["learning_rate"] = isinstance(config["learning_rate"], float)
+    ok["batch_size"] = isinstance(config["batch_size"], int)
+    ok["model_dir"] = isinstance(config["model_dir"], str)
+    ok["image_dir"] = isinstance(config["image_dir"], str)
+    ok["label_dir"] = isinstance(config["label_dir"], str)
+    ok["train_file"] = isinstance(config["train_file"], str)
+    ok["val_file"] = isinstance(config["val_file"], str)
+    ok["test_file"] = isinstance(config["test_file"], str)
 
     if return_invalid:
         return all(ok.values()), tuple(k for (k, v) in ok.items() if not v)
